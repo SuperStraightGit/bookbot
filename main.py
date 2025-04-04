3d4b24af6fbae7741f1a1bfb367b163d43b676e0
SuperStraightGit <evanbarker1+github@gmail.com>
2025-04-04 02:17:51 -0400

    finished guided bookbot project, ty boot.dev

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from stats import number_of_words, count_characters, convert_to_list_of_dictionaries
 
 def get_book_text(path):
@@ -6,14 +7,18 @@ def get_book_text(path):
         file_contents = f.read()
     return file_contents
 
-def book_metadata_report(book_file):
+def print_usage_and_exit():
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+def book_metadata_report(book_path):
 
     script_directory = os.path.dirname(os.path.abspath(__file__))
     
-    text = get_book_text(script_directory + "/books/" + book_file)
+    text = get_book_text(script_directory + "/" + book_path)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at /books/" + book_file)
+    print("Analyzing book found at /" + book_path)
     print("----------- Word Count ----------")
 
     word_count = number_of_words(text)
@@ -37,6 +42,12 @@ def book_metadata_report(book_file):
         print(f"{char}: {count}")
 
 def main():
-    book_metadata_report("frankenstein.txt")
+
+    if len(sys.argv) != 2:
+        print_usage_and_exit()
+
+    argument = sys.argv[1]
+
+    book_metadata_report(argument)
 
 main()
